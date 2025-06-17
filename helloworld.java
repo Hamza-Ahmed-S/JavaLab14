5fb9a0c5a46bf49200d2f6110a70e6be5ee654ac
Hamza Ahmed Siddiqui <193824426+Hamza-Ahmed-S@users.noreply.github.com>
2025-06-17 12:22:43 +0500

    Updated message in feature-update branch

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Updated from feature branch!");
+        System.out.println("Updates in feature branch!");
     }
 }
